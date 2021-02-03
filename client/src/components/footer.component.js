acac8513ba12abd00df390cbc1b045230b42b858
bobrovsi1988 <bobrovsi1988@gmail.com>
2021-02-03 18:52:32 +0300

    fix: footer

--- client/src/components/footer.component.js
+++ client/src/components/footer.component.js
@@ -5,8 +5,8 @@ export default class Footer extends Component {
 
     render() {
         return (
-            <div className="d-flex container justify-content-between">
-                <img src={Logo} alt="rs scool" height="50px"/>
+            <div className="d-flex container justify-content-between mt-auto">
+                <a href="https://rs.school/js/" ><img src={Logo} alt="rs scool" height="50px"/></a>
                 <div>
                     <p>Bobrov Sergey</p>
                     <a href="https://github.com/bobrovsi1988/rs-clone">GitHub</a>
